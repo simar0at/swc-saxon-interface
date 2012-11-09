271e51c9ad65b17fa0c7f0777136edda7898f97f
Omar Siam <simar@gmx.net>
2012-11-10 00:09:52 +0100

    Start of some testing.

--- swc-saxon-interface/src/test/java/org/sweble/wikitext/saxon/AppTest.java
+++ swc-saxon-interface/src/test/java/org/sweble/wikitext/saxon/AppTest.java
@@ -1,5 +1,12 @@
 package org.sweble.wikitext.saxon;
 
+import java.io.File;
+import java.net.URL;
+
+import net.sf.saxon.Query;
+
+import de.fau.cs.osr.utils.StringUtils;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -28,11 +35,22 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
+    private static final String TESTS_DIR = "org/sweble/wikitext/saxon";
+    
     /**
-     * Rigourous Test :-)
+     * 
      */
-    public void testApp()
+    public void testConfigureSite()
     {
-        assertTrue( true );
+    	URL xQueryUrl = AppTest.class.getResource("/" + TESTS_DIR + "/testConfigureSite.xquery");
+    	String path = StringUtils.decodeUsingDefaultCharset(xQueryUrl.getFile());
+		final File file = new File(path);
+		try {
+			Query.main(new String[] {"-init:org.sweble.wikitext.saxon.AddWikiParserFunction", "-s:M:\\arzwiki-20120728-pages-articles.xml", "-q:" + xQueryUrl.getFile()});
+			assertTrue(true);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
---
+++ swc-saxon-interface/src/test/resources/org/sweble/wikitext/saxon/testConfigureSite.xquery
@@ -0,0 +1,20 @@
+xquery version "1.0";
+(:declare default element namespace "http://www.mediawiki.org/xml/export-0.7/";:)
+declare namespace mw="http://www.mediawiki.org/xml/export-0.7/";
+declare namespace swc="http://sweble.org/doc/site/tooling/sweble/sweble-wikitext";
+declare namespace ptk="http://sweble.org/doc/site/tooling/parser-toolkit/ptk-xml-tools";
+declare option saxon:output "method=xml";
+declare option saxon:output "indent=yes";
+
+declare function local:getIwPrefix($s as xs:string) as xs:string {
+    replace(replace($s, "http://", ""), "\..*$", "")
+};
+
+let $baseConfig := swc:configureSite("My English Wiki",
+"http://localhost/",
+"en",
+"en",
+true())
+
+return
+    $baseConfig
